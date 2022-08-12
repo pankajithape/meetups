4b16e1ca9c625f5daa69e6c1604625a39c02dd34
Pankaj Ithape <pankajithape95@gmail.com>
2022-08-12 14:02:47 +0530

    first commit

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,27 @@
+import { MongoClient } from "mongodb";
+
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://miaahepankaj:Durva@cluster0.idknr8a.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
